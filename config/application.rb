adcdfa174fa8af5bdcb01b24066ff2f2471ee648
Harley Trung <harley@bsa.edu.vn>
2016-03-24 20:46:06 +0700

    dont disable rspec generator. RailsAppComposer WTF!!

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,11 @@ module CoderStore
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
+        view_specs: true,
+        helper_specs: true,
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
