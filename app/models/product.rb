815c30ba56319e1c91fbbefb4e288afe0587f67b
Harley Trung <harley@bsa.edu.vn>
2016-08-17 17:37:56 +0700

    milestone 3: add image to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+  validates :name, presence: true
+
+  def image_path_or_image
+    image_path.presence || image
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125095428) do
+ActiveRecord::Schema.define(version: 20160817095859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151125095428) do
     t.integer  "weight"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
