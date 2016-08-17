d0bbf1581a53907fa450061e5bf228c12d9e3ca5
Harley Trung <harley@bsa.edu.vn>
2016-08-17 17:52:04 +0700

    milestone 4: file uploading

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :image_path)
+    params.require(:product).permit(:name, :description, :image_path, :image)
   end
 end
