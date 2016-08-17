fd83de30853018e67e53a061d6410c6e1e97ca73
Harley Trung <harley@bsa.edu.vn>
2016-08-17 16:19:38 +0700

    milestone 1: create a product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new product_params
+    if @product.save
+      redirect_to root_path, alert: 'Success.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :image_path)
+  end
+end
