922ad6f73576724b2c9c363faf4c304374938086
Tucq <tucq88@gmail.com>
2016-10-25 04:02:01 +0000

    Update config + DB config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #
+  # For Vagrant box
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
+  config.web_console.whiny_requests = false
 end
