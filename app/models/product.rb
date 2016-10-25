fa13127a9292736c68219fc66007915c1ee55cb7
Harley Trung <harley@bsa.edu.vn>
2016-10-25 20:37:10 +0700

    set image_path by default to prevent image_tag from crashing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  before_save :set_default_image_path
+  validates :name, presence: true
+
+  private
+
+  def set_default_image_path
+    self.image_path ||= "http://lorempixel.com/200/200/fashion"
+  end
 end
